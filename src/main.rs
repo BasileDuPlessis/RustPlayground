38d85c0459d4106cc8915c450ff530f960e7a3fc
Basile du Plessis <basile.duplessis@gmail.com>
2020-12-11 16:54:54 +0100

    Refactor into a for loop

--- src/main.rs
+++ src/main.rs
@@ -56,37 +56,31 @@ fn arabic_to_roman(input:usize)->String {
 
     let mut output = String::new();
 
-    let translate = |u:usize, a:[&str; 3]| {
-        match (u==4, u==9, u>=5) {
-            (true, _, _) => a[0].to_owned() + a[1],
-            (_, true, _) => a[0].to_owned() + a[2],
-            (_, _, true) => a[1].to_owned() + &a[0].repeat(u-5),
-            _ => a[0].repeat(u)
+    let translate = |u:usize, arr:&[Option<&str>; 3]| {
+        match (u==4, u==9, u>=5, arr[0], arr[1], arr[2]) {
+            (true, _, _, Some(a), Some(b), _) => a.to_owned() + b,
+            (_, true, _, Some(a), _, Some(c)) => a.to_owned() + c,
+            (_, _, true, Some(a), Some(b), _) => b.to_owned() + &a.repeat(u-5),
+            (_, _, _, Some(a), _, _) => a.repeat(u),
+            _ => "".to_owned()
         }
     };
 
+    let library = [
+        [Some("M"), None, None],
+        [Some("C"), Some("D"), Some("M")],
+        [Some("X"), Some("L"), Some("C")],
+        [Some("I"), Some("V"), Some("X")]
+    ];
 
-    if input>=1000 {
-        output = "M".repeat((input as f32/1000_f32).floor() as usize);
-    }
-    if input>=100 {
-        output = format!("{}{}", output,
-            translate(((input as f32/100_f32).floor() as usize) % 10, ["C", "D", "M"])
-        );
-    }
-    if input>=10 {
-        output = format!("{}{}", output,
-            translate(((input as f32/10_f32).floor() as usize) % 10, ["X", "L", "C"])
-        );
-    }
-    if input>0 {
-        output = format!("{}{}", output,
-            translate(input % 10, ["I", "V", "X"])
-        );
-    } else {
-        output = "".to_owned();
-    }
-   
+    for (i, lib) in library.iter().enumerate() {
+        let i = 10_usize.pow(library.len() as u32) / 10_usize.pow(i as u32);
+        if input >= i {
+            output = format!("{}{}", output,
+                translate(((input as f32/i as f32).floor() as usize) % 10, lib)
+            );
+        }
+    }   
     output
 }
 
@@ -98,9 +92,6 @@ fn fizzbuzz(n:u8) -> String {
         _ => n.to_string()
     }
 }
-
-
-
 
 #[test]
 fn test_arabic_to_roman() {
